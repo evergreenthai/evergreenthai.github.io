96e14d400bcb6d642ad70862e0c94f6ae328e919
evergreenthai <45836096+evergreenthai@users.noreply.github.com>
2021-10-27 03:22:02 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,40 +1,26 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo="guoyicong0904@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo,$subject,$txt,$headers);
+    header("Location: index.html?mailsend");
+
+}
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
 $recipient = "guoyicong0904@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
-
-echo'
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
-</body>
-</html>
-
-
-
-';
 
 
 ?>
